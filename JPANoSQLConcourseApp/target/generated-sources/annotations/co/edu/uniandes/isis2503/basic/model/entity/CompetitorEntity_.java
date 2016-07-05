e1c697440288f3203a9f8cb2c8d5fb9f278bd185
Luis Felipe Mendivelso Osorio <lf.mendivelso10@uniandes.edu.co>
2016-07-05 09:11:02 -0500

    Ajustes de Forma y Repositorio

--- JPANoSQLConcourseApp/target/generated-sources/annotations/co/edu/uniandes/isis2503/basic/model/entity/CompetitorEntity_.java
+++ JPANoSQLConcourseApp/target/generated-sources/annotations/co/edu/uniandes/isis2503/basic/model/entity/CompetitorEntity_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-07-05T08:36:21")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-07-05T09:09:08")
 @StaticMetamodel(CompetitorEntity.class)
 public class CompetitorEntity_ { 
 
